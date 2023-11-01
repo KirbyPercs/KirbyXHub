d1e358857a3a60542b958083cf595b1f12d6f854
KirbyXPercs <134020489+KirbyPercs@users.noreply.github.com>
2023-10-31 20:31:32 -0700

    Update and rename Mod.lua to Whitelist.lua

---
+++ Whitelist.lua
@@ -0,0 +1,8 @@
+local HWIDTable = loadstring(game:HttpGet("https://pastebin.com/raw/m2e6NXsG"))()
+local HWID = game:GetService("RbxAnalyticsService")GetClientId()
+for i,v in pairs(HWIDTable) do
+    if v == HWID then
+print('Script is loading...')
+        
+    end
+end
